35b961a8a8b288b28f5ce33c993c2fa7efc65bd1
UchitProgrammer <uchit.programmer@gmail.com>
2022-05-13 13:16:01 +0530

    first commit

--- myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User,Course,Questions,Results,Faculty_Subject,Contact
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Course)
-admin.site.register(Questions)
-admin.site.register(Results)
-admin.site.register(Faculty_Subject)
-admin.site.register(Contact)
